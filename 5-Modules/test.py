7449f3d30a402c3ca59da5558fd2545bbbedc777
Shreyas Burji <shreyasburji1817@gmail.com>
2026-09-04 13:50:54 +0530

    Add 5-Modules

---
+++ 5-Modules/test.py
@@ -0,0 +1,6 @@
+from package.maths import *
+
+print(addition(5, 3))  # call the addition function
+print(subtraction(5, 3))  # call the subtraction function
+print(multiplication(5, 3))  # call the multiplication function
+print(division(5, 3))  # call the division function
